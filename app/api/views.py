6847cc0ab0955a44e451b1efc9efe1d21e6e83b9
AriyaD2003 <132356470+AriyaD2003@users.noreply.github.com>
2023-05-15 21:26:21 +0500

    Update views.py

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,5 @@
-import logging
 from flask import Blueprint, jsonify
+
 from app.posts.dao.posts_dao import PostsDAO
 from app.posts.dao.comments_dao import CommentsDAO
 from config import POST_PATH, COMMENTS_PATH
@@ -9,17 +9,14 @@ api_blueprint = Blueprint('api_blueprint', __name__)
 posts_dao = PostsDAO(POST_PATH)
 comments_dao = CommentsDAO(COMMENTS_PATH)
 
-logger = logging.getLogger("basik")
 
 @api_blueprint.route('/api/posts/')
-def posts_all():
-    logger.debug("Запрошенны все посты через API")
+def posts_api():
     posts = posts_dao.get_all()
-    return jsonify(posts)
+    return jsonify(posts), 200
 
 
-@api_blueprint.route('/api/posts/<int:post_pk>/')
-def posts_one(post_pk):
-    logger.debug("Запрошен пост с pk {post_pk} через API")
-    post = posts_dao.get_by_pk(post_pk)
-    return jsonify({"content": "Страничка одного поста"})
+@api_blueprint.route('/api/posts/<int:post_id>')
+def single_post_api(post_id):
+    post = posts_dao.get_by_pk(post_id)
+    return jsonify(post), 200
